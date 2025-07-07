0f95607e2cc0b16ccb8fbabff4fba216453c6dbf
olenevoid <mazekunov@gmail.com>
2025-07-07 11:11:05 +0500

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -1,19 +1,25 @@
 import os
+from dotenv import load_dotenv
+from os import environ
+
+
+load_dotenv()
+
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'HOST': environ['DB_HOST'],
+        'PORT': environ['DB_PORT'],
+        'NAME': environ['DB_NAME'],
+        'USER': environ['DB_USER'],
+        'PASSWORD': environ['DB_PASSWORD'],
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = environ['SECRET_KEY']
 
 DEBUG = True
 
