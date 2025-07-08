146e7d3ecb0ced8af9e6f7ad31ea3465fdb13418
olenevoid <mazekunov@gmail.com>
2025-07-08 10:05:35 +0500

    Delete unused import

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,4 +1,3 @@
-from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 from utils.time_utils import get_duration, format_duration, is_over_time_limit
