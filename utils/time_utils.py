992f4a0c1a5b1503e3120c5249accf48ee89a5f9
olenevoid <mazekunov@gmail.com>
2025-07-09 16:04:37 +0500

    Remove still_inside parameter from fromat_duration

--- utils/time_utils.py
+++ utils/time_utils.py
@@ -16,20 +16,12 @@ def get_duration(
     return duration
 
 
-def format_duration(
-        duration: timedelta,
-        still_inside: bool = False
-    ) -> str:
-    
-    still_inside_text = ''
+def format_duration(duration: timedelta) -> str:
     total_seconds = int(duration.total_seconds())
     total_minutes, seconds = divmod(total_seconds, 60)
     hours, minutes = divmod(total_minutes, 60)
 
-    if still_inside:
-        still_inside_text = 'Всё еще внутри'
-
-    return f'{hours}:{minutes:02}:{seconds:02} {still_inside_text}'
+    return f'{hours}:{minutes:02}:{seconds:02}'
 
 
 def is_over_time_limit(
